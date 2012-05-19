58977f3bda4ba39a60cdc6345dbc0e8634dc25a3
Victor.Pinto <Victor.Pinto@dclick.com.br>
2012-05-19 20:59:52 -0300

    logica de convolucao

--- src/photochopp/Convolucao.java
+++ src/photochopp/Convolucao.java
@@ -1,6 +1,21 @@
 package photochopp;
 
+import java.awt.image.BufferedImage;
+
 public abstract class Convolucao 
 {
-	
+	public void aplicarFiltro(float[][] filtro, BufferedImage imagem)
+	{
+		int imgWidth = imagem.getWidth();
+		int imgHeight = imagem.getHeight();
+		
+		for(int imgW = 0; imgW< imgWidth; imgW++)
+			for(int imgH = 0; imgH < imgHeight; imgH++)
+			{
+				int result = 0;
+				for(int filW = 0; filW < filtro.length; filW++);
+					for(int filH = 0; filH < filtro[filW].length; filH++)
+						
+			}
+	}
 }
